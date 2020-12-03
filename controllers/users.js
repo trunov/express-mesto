f8c18acfcfc1baddd9d5623d8334eb3d55253c5d
trunov <k.trunov.k@yandex.ru>
2020-12-03 12:46:39 +0200

    fix: patch controllers

--- controllers/users.js
+++ controllers/users.js
@@ -47,7 +47,7 @@ module.exports.createUser = (req, res) => {
 
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.params.id, { name, about }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
     .then((user) => {
       if (!user) {
         res.status(404).send({ message: "Нет пользователя с таким id" });
@@ -66,7 +66,7 @@ module.exports.updateUser = (req, res) => {
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.params.id, { avatar }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
     .then((user) => {
       if (!user) {
         res.status(404).send({ message: "Нет пользователя с таким id" });
